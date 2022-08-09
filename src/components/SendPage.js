567df037930d879a9afea097c341f85ea3b0686e
Henry Salas <henry@therefinery.com.au>
2022-08-09 21:32:19 +1000

    controlled form

--- src/components/SendPage.js
+++ src/components/SendPage.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function SendPage()
 {
+    const [recipe, setRecipe] = useState({
+        title:'',
+        instructions: ''
+    });
+
+    function onSubmitForm(e)
+    {
+        e.preventDefault();
+        const temp = {
+            title: recipe.title,
+            instructions: recipe.instructions
+        }
+        fetch(
+            "http://localhost:4000/drinks",
+            {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(temp)
+            }
+        )
+        .then(r=>r.json())
+        .then((data)=>{
+            window.location.reload();
+        })
+    }
+
+    function SetText(e)
+    {
+        setRecipe({
+           ...recipe,
+           [e.target.name]: e.target.value
+        });
+    }
+
     return <div>
-        Send page
+        <form onSubmit={onSubmitForm}>
+            <label>Title</label>
+            <input
+                value={recipe.title}
+                type="text"
+                name="title"
+                onChange={(e)=>SetText(e)}
+                default="Recipe title"
+             />
+            <br/>
+            <label>Instructions</label>
+            <textarea
+                value={recipe.instructions}
+                name="instructions"
+                onChange={(e)=>SetText(e)}
+            >
+            </textarea>
+            <br/>
+            <button type="submit">Submit Recipe</button>
+        </form>
     </div>;
 }
 
